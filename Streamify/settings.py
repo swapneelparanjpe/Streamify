e502940f688197fee3c81c85aa98837d99655d6d
swapneelparanjpe <swapneel.paranjpe@gmail.com>
2025-01-18 15:51:57 -0700

    Minor change

--- Streamify/settings.py
+++ Streamify/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f!gms(el$dx$-cpy(@+xe5j#-=9o*&)(@1ly7cd)iw)6a+%82%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
